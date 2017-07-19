f4f7e8c1e5e6655c9ff946917349f61dad6a9299
zhangxiaobei <zhangxiaobei99@126.com>
2017-07-19 15:34:16 +0800

    更换ViewPagerAnimActivity逻辑，用户可选择查看所有的切换效果

--- app/src/main/java/com/example/liuliu/xi/cityofanimation/activity/ViewPagerAnimActivity.java
+++ app/src/main/java/com/example/liuliu/xi/cityofanimation/activity/ViewPagerAnimActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,25 +20,36 @@ import com.example.liuliu.xi.cityofanimation.impl.ZoomOutTransform;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ViewPagerAnimActivity extends AppCompatActivity {
+public class ViewPagerAnimActivity extends AppCompatActivity implements View.OnClickListener {
     private ViewPager mViewPager;
     private List<View> mPagerAdapterDatas = new ArrayList<>();
     private int[] imageResId = {R.drawable.audrey_hepburn_01, R.drawable.audrey_hepburn_02, R.drawable.audrey_hepburn_03};
+    private Button mStyle1;
+    private Button mStyle2;
+    private Button mStyle3;
+    private Button mStyle4;
+    private Button mStyle5;
+    private Button mStyle6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_pager_anim);
         mViewPager = (ViewPager) findViewById(R.id.view_pager_anim_activity);
+        mStyle1 = (Button) findViewById(R.id.anim1_view_pager_activity);
+        mStyle2 = (Button) findViewById(R.id.anim2_view_pager_activity);
+        mStyle3 = (Button) findViewById(R.id.anim3_view_pager_activity);
+        mStyle4 = (Button) findViewById(R.id.anim4_view_pager_activity);
+        mStyle5 = (Button) findViewById(R.id.anim5_view_pager_activity);
+        mStyle6 = (Button) findViewById(R.id.anim6_view_pager_activity);
+        mStyle1.setOnClickListener(this);
+        mStyle2.setOnClickListener(this);
+        mStyle3.setOnClickListener(this);
+        mStyle4.setOnClickListener(this);
+        mStyle5.setOnClickListener(this);
+        mStyle6.setOnClickListener(this);
         setAdapterData();
         mViewPager.setAdapter(new PagerAdapter());
-        //mViewPager.setPageTransformer(true, new ZoomInTransform());//试试效果
-        //mViewPager.setPageTransformer(false, new ZoomInTransform());//试试效果
-        //mViewPager.setPageTransformer(true, new SimplePageTransform());//3D效果
-        //mViewPager.setPageTransformer(true, new DepthPageTransform());//透明度、大小变化
-        //mViewPager.setPageTransformer(true, new ZoomOutTransform());
-        mViewPager.setPageTransformer(true, new CubeTransformer(90));//3D效果
-
     }
 
     private void setAdapterData() {
@@ -49,6 +61,33 @@ public class ViewPagerAnimActivity extends AppCompatActivity {
             textView.setText(i + "");
             mPagerAdapterDatas.add(view);
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        mPagerAdapterDatas.clear();
+        setAdapterData();
+        switch (v.getId()) {
+            case R.id.anim1_view_pager_activity:
+                mViewPager.setPageTransformer(true, new ZoomInTransform());
+                break;
+            case R.id.anim2_view_pager_activity:
+                mViewPager.setPageTransformer(false, new ZoomInTransform());
+                break;
+            case R.id.anim3_view_pager_activity:
+                mViewPager.setPageTransformer(true, new SimplePageTransform());
+                break;
+            case R.id.anim4_view_pager_activity:
+                mViewPager.setPageTransformer(true, new DepthPageTransform());
+                break;
+            case R.id.anim5_view_pager_activity:
+                mViewPager.setPageTransformer(true, new ZoomOutTransform());
+                break;
+            case R.id.anim6_view_pager_activity:
+                mViewPager.setPageTransformer(true, new CubeTransformer(90));
+                break;
+        }
+        mViewPager.setAdapter(new PagerAdapter());
     }
 
     class PagerAdapter extends android.support.v4.view.PagerAdapter {
